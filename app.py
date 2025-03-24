cd43ba11d9216baa42f025ca39be70e1fbcfda16
ivan-emv <ivan.amador@europamundo.com>
2025-03-24 19:09:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -66,8 +66,8 @@ ciudad = st.text_input("Inserte Ciudad")
 trayecto = st.text_input("Inserte Trayecto")
 hora_presentacion = st.text_input("Inserte Hora de Presentación")
 hora_salida = st.text_input("Inserte Hora de Salida")
-punto_encuentro = st.text_area("Inserte Punto de Encuentro")
-direccion = st.text_input("Inserte Dirección")
+punto_encuentro = st.text_input("Inserte Punto de Encuentro")
+direccion = st.text_area("Inserte Dirección")
 
 # Validación de fecha y obtención del día y mes en texto
 try:
